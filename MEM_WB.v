eea9589c51ef67420b418c741c46d7329345f4b3
bclownin <157507360+bclownin@users.noreply.github.com>
2024-12-18 08:56:26 -0700

    Update MEM_WB.v
    added funct_o which had been erroneously removed

--- MEM_WB.v
+++ MEM_WB.v
@@ -21,41 +21,37 @@
 
 
 module MEM_WB(
-    //output reg [4:0] funct_o,
+    output reg [4:0] funct_o,
     output reg [7:0] immed_o, 
     output reg [7:0] memData_o, 
     output reg [7:0] ALUresult_o,
     output reg [2:0] targetReg_o, 
     output reg regWrite_o, 
-    //output reg zeroFlag_o, 
     input [7:0] immed,
     input [7:0] memData, 
     input [7:0] ALUresult,
     input [2:0] targetReg,
     input regWrite, 
-    //input zeroFlag, 
-    //input [4:0] funct,
     input jumpClear, 
+    input [4:0] funct,
     input clk
     );
     
     always @(posedge clk)begin
         if (!jumpClear)begin
-            //zeroFlag_o <= zeroFlag;
+            funct_o <= funct;
             ALUresult_o <= ALUresult;
             targetReg_o <= targetReg;
             regWrite_o <= regWrite;
             immed_o <= immed;
-            //funct_o <= funct;
             memData_o <= memData;
         end
         else  begin
-            //zeroFlag_o <= 0;
+            funct_o <= 0;
             ALUresult_o <= 0;
             targetReg_o <= 0;
             regWrite_o <= 0;
             immed_o <= 0;
-            //funct_o <= 0;
             memData_o <= 0;
         end
     end
